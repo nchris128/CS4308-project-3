831ec221daf57b800eb09eaa4efed94e1b9b5574
nchris128 <nchris128@gmail.com>
2022-04-26 16:21:08 -0400

    start.py

--- scl_parser.py
+++ scl_parser.py
@@ -1,20 +1,15 @@
-import scl_scanner
-import scl_lexier
-import scl_parser
-import scl_interpreter
-
-import os
+import scl_lexier 
+import scl_scanner 
 
 
 # open files that are inside the source folder and read
-
 keywordsCheck = scl_scanner.keywordScanner()
 operatorsCheck = ['=', '*', '/', '('')', '[]', '<=', ":"]
 identifiersCheck = scl_scanner.identifierScanner()
 identifierDuplicate = []
 fileName = ''
 
-def compiler():
+def compiler(fileName):
     readSCL = scl_scanner.read(fileName)
     cleanedText = scl_scanner.cleanup(readSCL)
     return cleanedText
@@ -74,108 +69,13 @@ def operators(text, nextword):
 def syntaxError():
     print('SYNTAX ERROR')
 
-#menu system to access all parts of parser
-class parserMenu():
-
-    def printMenu():
-        global fileName
-        print("========Menu========")
-        print("1) Choose File ")
-        print("2) Read File")
-        print("3) Scan File")
-        print("4) Parse File")
-        print("5) Run Translator")
-        print("6) Exit ")
-        print("====================")
-
-        #user input to navigate menu
-        choice = input()
-        if choice == '1':
-            fileName = parserMenu.chooseFile()
-            compiler()
-            parserMenu.printMenu()
-        elif choice == '2':
-            if fileName == '':
-                print("\nPlease select a file first")
-                parserMenu.printMenu()
-            parserMenu.readFilename()
-            parserMenu.printMenu()
-        elif choice == '3':
-            if fileName == '':
-                print("\nPlease select a file first")
-                parserMenu.printMenu()
-            global keywordsList
-            global identifiersList
-            global operatorsList
-            keywordsList, identifiersList, operatorsList = parserMenu.scanFile()
-            parserMenu.printMenu()
-        elif choice == '4':
-            if fileName == '':
-                print("\nPlease select a file first")
-                parserMenu.printMenu()
-            parserMenu.begin()
-            parserMenu.printMenu()
-        elif choice == '5':
-            print(keywordsList, identifiersList, operatorsList) 
-            if fileName == '':
-                print("\nPlease select a file first")
-                parserMenu.printMenu()
-            translate()
-            # put translator here...
-        elif choice == '6':
-            print("Exiting....")
-        else:
-            print("Wrong Choice, Try Again")
-
-    #choose scl file in working directory to use
-    def chooseFile():
-        global fileName
-        sclFiles = []
-        counter = 1
-        files = os.listdir(os.getcwd())
-        print("\nPlease put scl files in working directory and press the number for the file you would like to open:")
-        for scl in files:
-            if ".scl" in scl:
-                print("[" + str(counter) + "] " + scl)
-                sclFiles.append(scl)
-                counter += 1
-        choice = input()
-        fileNum = int(choice) - 1
-        fileName = sclFiles[fileNum]
-        return(fileName)
-
-    #reads file 
-    def readFilename():
-        # open files that are inside the source folder and read
-        print(scl_scanner.read(fileName))
-        print("\n===============================")
-        print("   End of File Name: " + fileName)
-        print("===============================\n")
-
-
-    def scanFile():
-        global keywordsCheck
-        global identifiersCheck
-        global operatorsCheck
-        readSCL = scl_scanner.read(fileName)
-        cleanedText = scl_scanner.cleanup(readSCL)
-        keywordsCheck = scl_scanner.keywordScanner()
-        identifiersCheck = scl_scanner.identifierScanner()
-        operatorsCheck = scl_scanner.operatorScanner()
-        scl_scanner.displayKeywords()
-        scl_scanner.displayIdentifiers()
-        scl_scanner.displayOperators()
-        return keywordsCheck, identifiersCheck, operatorsCheck
-
-    def begin():
-        start()
 
 
 
-def start():
+def startParse(fileName):
     index = 0
     indexAhead = index + 1
-    scl = compiler()
+    scl = compiler(fileName)
     scl.append("EOF")
 
     for each in scl:
@@ -188,12 +88,7 @@ def start():
         if indexAhead >= len(scl):
             break
 
-def translate():
-    print("Translating...")
-    scl = compiler()
-    scl_interpreter.scl_translator_printer(scl)
 
-parserMenu.printMenu()
 
 
 
---
+++ start.py
@@ -0,0 +1,97 @@
+
+import scl_interpreter
+import scl_lexier 
+import scl_scanner 
+import scl_parser 
+import os
+
+def printMenu():
+    global fileName
+    
+    print("========Menu========")
+    print("1) Choose File ")
+    print("2) Read File")
+    print("3) Scan File")
+    print("4) Parse File")
+    print("5) Interpret File")
+    print("6) Exit ")
+    print("====================")
+
+    #user input to navigate menu
+    choice = input()
+    if choice == '1':
+        fileName = chooseFile()
+        scl_parser.compiler(fileName)
+        printMenu()
+    elif choice == '2':
+        if fileName == '':
+            print("\nPlease select a file first")
+            printMenu()
+        readFilename()
+        printMenu()
+    elif choice == '3':
+        if fileName == '':
+            print("\nPlease select a file first")
+            printMenu()
+        scanFile()
+        printMenu()
+    elif choice == '4':
+        if fileName == '':
+            print("\nPlease select a file first")
+            printMenu()
+        begin(fileName)
+        printMenu()
+    elif choice == '5':
+        if fileName == '':
+            print("\nPlease select a file first")
+            printMenu()
+        begin()
+        printMenu()
+    elif choice == '6':
+        print("Exiting....")
+    else:
+        print("Wrong Choice, Try Again")
+
+#choose scl file in working directory to use
+def chooseFile():
+    global fileName
+    sclFiles = []
+    counter = 1
+    files = os.listdir(os.getcwd())
+    print("\nPlease put scl files in working directory and press the number for the file you would like to open:")
+    for scl in files:
+        if ".scl" in scl:
+            print("[" + str(counter) + "] " + scl)
+            sclFiles.append(scl)
+            counter += 1
+    choice = input()
+    fileNum = int(choice) - 1
+    fileName = sclFiles[fileNum]
+    return(fileName)
+
+#reads file 
+def readFilename():
+    # open files that are inside the source folder and read
+    print(scl_scanner.read(fileName))
+    print("\n===============================")
+    print("   End of File Name: " + fileName)
+    print("===============================\n")
+
+
+def scanFile():
+    global keywordsCheck
+    global identifiersCheck
+    global operatorsCheck
+    readSCL = scl_scanner.read(fileName)
+    cleanedText = scl_scanner.cleanup(readSCL)
+    keywordsCheck = scl_scanner.keywordScanner()
+    identifiersCheck = scl_scanner.identifierScanner()
+    operatorsCheck = scl_scanner.operatorScanner()
+    scl_scanner.displayKeywords()
+    scl_scanner.displayIdentifiers()
+    scl_scanner.displayOperators()
+
+def begin(fileName):
+    scl_parser.startParse(fileName)
+
+printMenu()
